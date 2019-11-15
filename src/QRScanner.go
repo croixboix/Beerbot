b9e1732d01cb842ffb176ec5526f2f4d9e87c6eb
Sam Jansen <sjansen@tsrsolutions.com>
2019-11-15 12:27:28 -0600

    Wrote test code for verifying and processing orders against REST API

--- src/QRScanner.go
+++ src/QRScanner.go
@@ -2,76 +2,152 @@ package main
 
 import(
   "fmt"
-  "bufio"
-  "os"
+  "encoding/json"
+  //"bufio"
+  //"os"
   "strings"
   "net/http"
   "io/ioutil"
 )
 
 var (
-
 )
 
-func requestOrders() string {
-
-  url := "http://96.30.245.134:3000/orders/index"
-  payload := strings.NewReader("username=test")
-  req, _ := http.NewRequest("GET", url, payload)
-  req.Header.Add("Authorization", "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE1NzM1ODI1MTh9.NbhB_MFF9pV7dBLBp8o5dKtY0EgMR3sK621hN34G1Tw")
-  req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-  req.Header.Add("User-Agent", "PostmanRuntime/7.19.0")
-  req.Header.Add("Accept", "/")
+//Verify that the order exists on the API order list
+func verifyOrder(uname string) []byte{
+  url := "http://96.30.245.134:3000/orders/verify"
+  payload := strings.NewReader("{\n\t\"order\": {\n\t\t\"username\": \""+uname+"\"\n\t}\n}")
+  req, _ := http.NewRequest("POST", url, payload)
+  req.Header.Add("Content-Type", "application/json")
+  req.Header.Add("Accept", "*/*")
   req.Header.Add("Cache-Control", "no-cache")
-  req.Header.Add("Postman-Token", "711bb03c-4ad6-4809-a89f-c372f1eaab81,af06cae4-ff57-4998-a9b5-26d004bb32d5")
+  req.Header.Add("Host", "96.30.245.134:3000")
   req.Header.Add("Accept-Encoding", "gzip, deflate")
-  req.Header.Add("Cookie", "_beer_bot_api_session=AJ03Hd2KhYHVoMe%2BiRmykVhp%2BdGRQ%2Fi%2BgElgQHFRdJUKEoST6pGtRJM%2Ft4wd9vbr8ZcK%2BlPLNPz8MtS3p3Er9AqCKjZS7By%2Bmx6PsjbxPyhUtQ3QwRHZ7i7eQsvmLs8o3IIVDBtw5PJVi57CN4FJZ5lCjqmXnQ08VfiyFCh4rMa7ixGRXxtIF7nINv7et78s7f9gTwve132c%2FPsYYPQ2gaLfHZ8oJKOjuyNTqoTqz23tssWqCPeiigUGZU6yMBE50J%2FcsTYKTZDVDopqHppc61MkXdzjEAnAubwvp5R%2Fljr0eRa%2BPbnBJNKyptIKIJOWRhd0oM8%3D--ELRctXZO7nN6Z8bp--LMiKintBHTmbRTV7R76NHg%3D%3D")
-  req.Header.Add("Referer", "http://96.30.245.134:3000/orders/create")
+  req.Header.Add("Content-Length", "39")
   req.Header.Add("Connection", "keep-alive")
   req.Header.Add("cache-control", "no-cache")
   res, _ := http.DefaultClient.Do(req)
   defer res.Body.Close()
   body, _ := ioutil.ReadAll(res.Body)
-  fmt.Println(res)
-  fmt.Println(string(body))
+  //fmt.Println(res)
+  //fmt.Println(string(body))
+  return body
 
+  /*
+    Order does exist response:
+    {
+      "id": 591,
+      "username": "test",
+      "created_at": "2019-11-15T16:31:21.321Z",
+      "updated_at": "2019-11-15T16:31:21.321Z",
+      "url": "http://96.30.245.134:3000/orders/591.json"
+    }
 
-  return string(body)
+    Order does not exist response:
+    {
+  	"id": null,
+  	"username": null,
+  	"created_at": null,
+  	"updated_at": null,
+  	"url": null
+    }
+  */
 }
 
-
-func processOrders() {
-  //If sucessful return 0, else 1
-  
+// Tells API that order processed and deletes order from API order list
+func processOrder(uname string) []byte{
   url := "http://96.30.245.134:3000/orders/processed"
-  payload := strings.NewReader("username=test")
-  req, _ := http.NewRequest("POST", url, payload)
-  req.Header.Add("Authorization", "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE1NzM1ODI1MTh9.NbhB_MFF9pV7dBLBp8o5dKtY0EgMR3sK621hN34G1Tw")
-  req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-  req.Header.Add("User-Agent", "PostmanRuntime/7.19.0")
-  req.Header.Add("Accept", "/")
-  req.Header.Add("Cache-Control", "no-cache")
-  req.Header.Add("Postman-Token", "711bb03c-4ad6-4809-a89f-c372f1eaab81,606f5a7f-3ce3-4f71-8161-4b387d14df1e")
-  req.Header.Add("Accept-Encoding", "gzip, deflate")
-  req.Header.Add("Cookie", "_beer_bot_api_session=AJ03Hd2KhYHVoMe%2BiRmykVhp%2BdGRQ%2Fi%2BgElgQHFRdJUKEoST6pGtRJM%2Ft4wd9vbr8ZcK%2BlPLNPz8MtS3p3Er9AqCKjZS7By%2Bmx6PsjbxPyhUtQ3QwRHZ7i7eQsvmLs8o3IIVDBtw5PJVi57CN4FJZ5lCjqmXnQ08VfiyFCh4rMa7ixGRXxtIF7nINv7et78s7f9gTwve132c%2FPsYYPQ2gaLfHZ8oJKOjuyNTqoTqz23tssWqCPeiigUGZU6yMBE50J%2FcsTYKTZDVDopqHppc61MkXdzjEAnAubwvp5R%2Fljr0eRa%2BPbnBJNKyptIKIJOWRhd0oM8%3D--ELRctXZO7nN6Z8bp--LMiKintBHTmbRTV7R76NHg%3D%3D")
-  req.Header.Add("Referer", "http://96.30.245.134:3000/orders/create")
-  req.Header.Add("Connection", "keep-alive")
-  req.Header.Add("cache-control", "no-cache")
-  res, _ := http.DefaultClient.Do(req)
-  defer res.Body.Close()
-  body, _ := ioutil.ReadAll(res.Body)
-  fmt.Println(res)
-  fmt.Println(string(body))
-  
+
+	payload := strings.NewReader("{\n\t\"order\": {\n\t\t\"username\": \""+uname+"\"\n\t}\n}")
+
+	req, _ := http.NewRequest("POST", url, payload)
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("User-Agent", "PostmanRuntime/7.19.0")
+	req.Header.Add("Accept", "*/*")
+	req.Header.Add("Cache-Control", "no-cache")
+	req.Header.Add("Postman-Token", "aa411b8b-f735-484e-9f7c-2a871763a9dc,7b8a5042-c09d-47b0-a4ea-68ba0a8dda7a")
+	req.Header.Add("Host", "96.30.245.134:3000")
+	req.Header.Add("Accept-Encoding", "gzip, deflate")
+	req.Header.Add("Content-Length", "39")
+	req.Header.Add("Connection", "keep-alive")
+	req.Header.Add("cache-control", "no-cache")
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	//fmt.Println(res)
+	//fmt.Println(string(body))
+
+
+  return body
 }
 
 
 func main() {
 
-  orders := requestOrders()
+  type verifyResponse struct {
+    ID int `json:"id"`
+    Username string `json:"username"`
+    CreatedAt string `json:"created_at"`
+    UpdatedAt string  `json:"updated_at"`
+    URL string  `json:"url"`
+  }
 
-  fmt.Println("requestOrders Return: ", string(orders))
+  type processResponse struct {
+    Processed bool `json:"processed"`
+  }
 
+  var user string = "test"
+
+  fmt.Println("Verify Order")
+
+  var response []byte = verifyOrder(user)
+  var verifyData verifyResponse
+
+  err := json.Unmarshal(response, &verifyData)
+  if err != nil {
+        fmt.Println("error:", err)
+    }
+
+  fmt.Println("Verify Order Response Dump:")
+  fmt.Println("id: ", verifyData.ID)
+  fmt.Println("username: ", verifyData.Username)
+  fmt.Println("created_at: ", verifyData.CreatedAt)
+  fmt.Println("updated_at: ", verifyData.UpdatedAt)
+  fmt.Println("url: ", verifyData.URL)
+
+
+  if(verifyData.Username != "null"){
+
+    fmt.Println("Process/Delete Order")
+
+    var processData processResponse
+
+    var processResponse []byte = processOrder(verifyData.Username)
+
+    err := json.Unmarshal(processResponse, &processData)
+    if err != nil {
+          fmt.Println("error:", err)
+      }
+
+    fmt.Println("Process Order Response Dump:")
+    fmt.Println("processed: ", processData.Processed)
+
+    if(processData.Processed == true){
+      //Call pour
+      fmt.Println("ORDER PROCESSED, LET USER POUR")
+    } else{
+      fmt.Println("ORDER DOES NOT EXIST, DO NOT LET USER POUR")
+    }
+  }
+
+
+
+
+  /*
   scanner := bufio.NewScanner(os.Stdin)
 
   for scanner.Scan() {
@@ -85,6 +161,7 @@ func main() {
   if err := scanner.Err(); err != nil {
     fmt.Fprintln(os.Stderr, "reading standard input:", err)
   }
-  
+  */
+
 
 }
