35c2dfee5ab40913a03ccba98cc9c75135479682
Sam Jansen <jansens@msoe.edu>
2020-10-21 17:12:33 -0500

    fixed some stuff with formatting

--- src/BeerBot_Main.go
+++ src/BeerBot_Main.go
@@ -217,25 +217,22 @@ func main() {
 	testOrder := newOrder(user, testTapOrder)
 
 
-			//This is just a timeout function so that the program will timeout and run gpio.Close() below
-		 	c1 := make(chan string, 1)
-
-
-		     // Run your long running function in it's own goroutine and pass back it's
-		     // response into our channel.
-		     go func() {
-		         togglePour(*testOrder)
-						 text := "togglePour Finished!"
-		         c1 <- text
-		     }()
-
-		     // Listen on our channel AND a timeout channel - which ever happens first.
-		     select {
-		     case res := <-c1:
-		         fmt.Println(res)
-		     case <-time.After(20 * time.Second):
-		         fmt.Println("out of time :(")
-		     }
+	//This is just a timeout function so that the program will timeout and run gpio.Close() below
+	c1 := make(chan string, 1)
+  // Run your long running function in it's own goroutine and pass back it's
+	 // response into our channel.
+	go func() {
+		togglePour(*testOrder)
+		text := "togglePour Finished!"
+		c1 <- text
+		}()
+	// Listen on our channel AND a timeout channel - which ever happens first.
+	select {
+		case res := <-c1:
+			fmt.Println(res)
+  	case <-time.After(20 * time.Second):
+		  fmt.Println("out of time :(")
+		}
 
 	/* API test code below
 
