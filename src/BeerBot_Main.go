06514e323075c17953e9d678f48dd2de673e602d
Sam Jansen <jansens@msoe.edu>
2020-11-02 17:44:44 -0600

    Websocket echo test code added

--- src/BeerBot_Main.go
+++ src/BeerBot_Main.go
@@ -17,6 +17,7 @@ import (
 	"os"
 	"strings"
 	"sync"
+	"github.com/sacOO7/gowebsocket"
 )
 
 const (
@@ -97,6 +98,49 @@ func main() {
 	}
 
 
+	//Websocket echo test
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	socket := gowebsocket.New("ws://echo.websocket.org/")
+
+	socket.OnConnectError = func(err error, socket gowebsocket.Socket) {
+		log.Fatal("Received connect error - ", err)
+	}
+
+	socket.OnConnected = func(socket gowebsocket.Socket) {
+		log.Println("Connected to server");
+	}
+
+	socket.OnTextMessage = func(message string, socket gowebsocket.Socket) {
+		log.Println("Received message - " + message)
+	}
+
+	socket.OnPingReceived = func(data string, socket gowebsocket.Socket) {
+		log.Println("Received ping - " + data)
+	}
+
+    socket.OnPongReceived = func(data string, socket gowebsocket.Socket) {
+		log.Println("Received pong - " + data)
+	}
+
+	socket.OnDisconnected = func(err error, socket gowebsocket.Socket) {
+		log.Println("Disconnected from server ")
+		return
+	}
+
+	socket.Connect()
+
+  socket.SendText("Thoughtworks guys are awesome !!!!")
+
+	for {
+		select {
+		case <-interrupt:
+			log.Println("interrupt")
+			socket.Close()
+			return
+		}
+	}
 
 
 
@@ -136,6 +180,11 @@ func main() {
 	gpio.Close()
 
 }
+
+
+
+
+
 
 
 
