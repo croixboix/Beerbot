52db38682c87be90b6b21ccea4005de6d870f60d
Sam Jansen <jansens@msoe.edu>
2021-01-18 17:11:05 -0600

    Safe point before trying ways to fullfill multiple orders at once

--- src/BeerBot_Main.go
+++ src/BeerBot_Main.go
@@ -104,7 +104,8 @@ func main() {
 		time.Sleep(1*time.Second)
 
 		//Check order queue for orders to pull
-		var userOrders []Order = getOrders(tapUUID)
+		userOrders := getOrders(tapUUID)
+		//var userOrders []Order = getOrders(tapUUID)
 
 		//If there are orders to serve then let us fullfill them
 		if orderQueueSize > 1 {
