b52cadf9fea67e1aa85119d8a3454de3373ffe7a
Sam Jansen <jansens@msoe.edu>
2021-01-18 16:47:50 -0600

    debugging getOrders

--- src/BeerBot_Main.go
+++ src/BeerBot_Main.go
@@ -215,7 +215,7 @@ func processOrder(int orderID) bool {
 	*/
 
 	url := "http://96.30.244.56:3000/api/v1/tap_orders"
-	payload := strings.NewReader("{\n\t\"order\": {\n\t\t\"username\": \"" + uname + "\"\n\t}\n}")
+	payload := strings.NewReader("{\n\t\"order\": {\n\t\t\"username\": \"" + orderID + "\"\n\t}\n}")
 	req, _ := http.NewRequest("POST", url, payload)
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Accept", "*/*")
